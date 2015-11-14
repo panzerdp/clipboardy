9129feae8b09f9479197fb8c74be8a9ef88f6e1f
Dmitri Pavlutin <dpavlutin@gmail.com>
2015-11-14 16:23:38 +0200

    Show the buttons in group

--- src/js/common/const.js
+++ src/js/common/const.js
@@ -5,5 +5,9 @@ module.exports = {
   MESSAGE_DISPLAY_COLLAPSE: 'message_display_collapse',
   MESSAGE_TOGGLE_SOURCE_COLLAPSE: 'message_toggle_source_collapse',
 
-  MIN_HEIGHT_FOR_DISPLAYING_COLLAPSE: 150 //Minimum height of the source for displaying the collapse button
+  MIN_HEIGHT_FOR_DISPLAYING_COLLAPSE: 150, //Minimum height of the source for displaying the collapse button
+
+  SETTING_BUTTONS_LAYOUT: 'setting_buttons_layout',
+  VALUE_BUTTONS_LAYOUT_TOP: 'value_buttons_layout_top',
+  VALUE_BUTTONS_LAYOUT_LEFT: 'value_buttons_layout_left'
 };
