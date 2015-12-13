185b05f8878bd1746cd931242f0d98124030a201
Dmitri Pavlutin <dpavlutin@gmail.com>
2015-12-13 14:45:43 +0200

    Buttons appearance config option

--- src/js/options/controllers/options.js
+++ src/js/options/controllers/options.js
@@ -5,7 +5,19 @@
 function OptionsCtrl(Message, C, Storage) {
   "ngInject"
   var self = this;
+  self.buttonsAppearance = false;
+  self.C = C;
 
+  self.onButtonsAppearanceChange = onButtonsAppearanceChange;
+
+  // Loading settings data from storage
+  Storage.get(C.SETTING_BUTTONS_APPEARANCE, C.VALUE_BUTTONS_APPEARANCE_ALWAYS).then(function(buttonsAppearance) {
+    self.buttonsAppearance = buttonsAppearance;
+  });
+
+  function onButtonsAppearanceChange() {
+    Storage.set(C.SETTING_BUTTONS_APPEARANCE, self.buttonsAppearance);
+  }
 }
 
 module.exports = [
