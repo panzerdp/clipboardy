a3b5fefdbd46c9690e60cd32f7e8005913db89f3
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-03-31 19:17:02 +0300

    Use native hover events

--- src/js/common/source/behaviors/appearance.js
+++ src/js/common/source/behaviors/appearance.js
@@ -5,12 +5,18 @@ module.exports = inherit(Base, {
 
   initialize: function() {
     var self = this;
-    self.getIframeContainer().addClass('clipboardy-hidden');
-    self.getSource().add(self.getIframeContainer()).hover(function() {
-      self.getIframeContainer().removeClass('clipboardy-hidden');
-    }, function() {
-      self.getIframeContainer().addClass('clipboardy-hidden');
-    });
+    var iframeContainer = self.getIframeContainer();
+    iframeContainer.classList.add('clipboardy-hidden');
+    function onMouseOver() {
+      iframeContainer.classList.remove('clipboardy-hidden');
+    }
+    function onMouseOut() {
+      iframeContainer.classList.add('clipboardy-hidden');
+    }
+    self.getSource().addEventListener('mouseover', onMouseOver);
+    self.getSource().addEventListener('mouseout', onMouseOut);
+    iframeContainer.addEventListener('mouseover', onMouseOver);
+    iframeContainer.addEventListener('mouseout', onMouseOut);
   }
 
 }, {
